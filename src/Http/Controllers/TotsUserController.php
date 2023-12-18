6098afe1762b8ae24077232568a5c4bfdedf741d
Matias Camiletti <matias.camiletti@gmail.com>
2023-12-18 11:55:24 -0300

    Add user controller first structure.

---
+++ src/Http/Controllers/TotsUserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Tots\Auth\Models\TotsUser;
+
+class TotsUserController extends \Illuminate\Routing\Controller
+{
+    use ValidatesRequests;
+
+    public function list(Request $request)
+    {
+        // Create query
+        $elofilter = \Tots\EloFilter\Query::run(TotsUser::class, $request);
+        // Execute query
+        return $elofilter->execute();
+    }
+}
