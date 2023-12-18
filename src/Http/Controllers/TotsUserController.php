f19578125906f043364afbe7e9f0b926765614e8
Matias Camiletti <matias.camiletti@gmail.com>
2023-12-18 12:38:06 -0300

    Add update method.

--- src/Http/Controllers/TotsUserController.php
+++ src/Http/Controllers/TotsUserController.php
@@ -5,11 +5,20 @@ namespace Tots\User\Http\Controllers;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Tots\Auth\Models\TotsUser;
+use Tots\Auth\Repositories\TotsUserRepository;
+use Tots\User\Http\Requests\TotsUserRequest;
 use Tots\User\Http\Resources\TotsUserResource;
 
 class TotsUserController extends \Illuminate\Routing\Controller
 {
     use ValidatesRequests;
+
+    protected $userRepository;
+
+    public function __construct(TotsUserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
     public function list(Request $request)
     {
@@ -25,5 +34,12 @@ class TotsUserController extends \Illuminate\Routing\Controller
         $item = TotsUser::findOrFail($id);
         // Execute query
         return response()->json(TotsUserResource::make($item));
+    }
+
+    public function update($id, TotsUserRequest $request)
+    {
+        $this->userRepository->updateByData($id, $request->validated());
+        // Execute query
+        return response()->json(['message' => 'User updated']);
     }
 }
---
+++ src/Http/Requests/TotsUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tots\User\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TotsUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'firstname' => ['string', 'max:255', 'nullable'],
+            'lastname' => ['string', 'max:255', 'nullable'],
+            'email' => ['required', 'email'],
+            'photo' => ['string', 'nullable'],
+            'phone' => ['string', 'max:50', 'nullable'],
+        ];
+    }
+}
